f2632885d574ac69a1c99d5b49b3d7b12bedc262
enelesmai <xochitl.selene.fp@gmail.com>
2021-04-19 18:04:52 -0500

    Render user associated to the comment

--- discuss/web/static/js/socket.js
+++ discuss/web/static/js/socket.js
@@ -21,9 +21,16 @@ const createSocket = (topicId) => {
 }
 
 function commentTemplate(comment){
+  let email = "Anonymous";
+  if (comment.user) {
+    email = comment.user.email;
+  }
   return `
   <li class="collection-item">
     ${comment.content}
+    <div class="secondary-content">
+      ${email}
+    </div>
   </li>
 `;
 }
